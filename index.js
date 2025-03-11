df5ee2ec1774d813e0db0fc68fb72ebc85894e77
Kannishree <121699553+Kannishree@users.noreply.github.com>
2025-03-11 20:40:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
